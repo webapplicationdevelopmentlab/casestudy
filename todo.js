e7130c430ab1bd1ad77cad54c112a3c634e06a9c
Aswathi C <ashwathichuliyar@gmail.com>
2021-12-04 15:26:16 +0530

    case study #01

---
+++ todo.js
@@ -0,0 +1,89 @@
+//redirect if not valid login
+let currentUser=JSON.parse(window.localStorage.getItem('user'));
+if(!currentUser){
+    window.location='ToDoListlogin.html';
+
+}
+
+// setting welcome text
+$('#welcomeText').html(`Hello user! <br> Welcome to your ToDo List`);
+
+// code for log out
+$('#logOut').on('click',(e)=>{
+    e.preventDefault();
+    // Sign-out successful.
+    window.localStorage.removeItem('user');
+    window.location="ToDoListlogin.html";
+    console.log('logout successfully');
+})
+
+// AJAX call to fetch data using axios library.
+const getList=async ()=>{
+    try{
+        const res=await axios.get('https://jsonplaceholder.typicode.com/todos');
+        const lists=res.data;
+        console.log(lists);
+        let listcontent='';
+        lists.forEach((el,index)=>{
+            listcontent+=`<li class="list-group-item ${el.completed?'disabledList':''} ${index%2?'list-group-item-info':'list-group-item-success'}"> <input type="checkbox" class="checkbox" ${el.completed?' checked':''}/> <label for=""> ${el.title}</label></li>`
+        });
+        $('#todoList').html(listcontent);
+        if(checkedCount){
+            checkedCount=0;
+        }
+
+    }
+    catch(e){
+        console.log('failed to fetch lists data',e);
+    }
+}
+// call getlist() when GET LIST is clicked.
+$('#getList').on('click',(e)=>{
+    e.preventDefault();
+    getList();
+});
+
+//variable to keep track of cheking list items
+let checkedCount=0;
+
+const alertPromise= ()=>{
+     return new Promise((resolve,reject)=>{
+
+         
+        if(checkedCount===5){
+            resolve(checkedCount)
+        }
+        else{
+            reject('count not equal to 5');
+        }
+    });
+}
+
+const promiseCall=()=>{
+    alertPromise().then((data)=>{
+        alert(`Congrats. ${data} Tasks have been Successfully Completed `);
+    })
+    .catch((err)=>{
+        console.log('promise rejected');
+    })
+}
+
+
+getList();
+
+$('#todoList').on('change','.checkbox',function(e){
+    if($(this).prop('checked')===true){
+        console.log('checked');
+        checkedCount++; 
+        $(this).parent().addClass('active');
+    }
+    else{
+        checkedCount--;
+        console.log('unchecked');
+        $(this).parent().removeClass('active');
+    }
+    
+    promiseCall();
+
+
+});
